c7be396995b83d4fc4ec36ca87ef83e23b938a93
Casey6624 <34159052+Casey6624@users.noreply.github.com>
2019-02-24 18:14:06 +0000

    First Commit - some endpoints setup, not yet linked to DB

---
+++ TasksAPI/TasksAPI/Controllers/TaskController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TasksAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TasksAPI.Controllers
+{
+   [Route("api/[controller]")]
+   [ApiController]
+   public class TaskController : ControllerBase
+    {
+        private readonly TaskContext _context;
+
+        public TaskController(TaskContext context)
+        {
+            _context = context;
+            if(_context.TaskItems.Count() == 0)
+            {
+                _context.TaskItems.Add(new TaskItem { assignedTo = "Casey" });
+                _context.SaveChanges();
+            }
+        }
+        // GET api/tasks
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TaskItem>>> GetTaskItems()
+        {
+            return await _context.TaskItems.ToListAsync();
+        }
+
+        // GET api/tasks/{id}
+        [HttpGet("{taskID}")]
+        public async Task<ActionResult<TaskItem>> GetTaskItem(long taskID)
+        {
+            var taskItem = await _context.TaskItems.FindAsync(taskID);
+
+            if (taskItem == null)
+            {
+                return NotFound();
+            }
+
+            return taskItem;
+        }
+
+        // POST: api/Task
+        [HttpPost]
+        public async Task<ActionResult<TaskItem>> PostTaskItem(TaskItem item)
+        {
+            _context.TaskItems.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTaskItem), new { taskID = item.taskID }, item);
+        }
+    }
+}
---
+++ TasksAPI/TasksAPI/Models/TaskItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TasksAPI.Models
+{
+    public class TaskItem
+    {
+        // [key] from ComponentModel.DataAnnotations is needed as .Net assumes PK is "Id"
+        [Key]
+        public long taskID { get; set; }
+        public string assignedTo { get; set; }
+        public int priority { get; set; }
+        public string taskTitle { get; set; }
+        public string taskDescription { get; set; }
+        public string taskTime { get; set; }
+        public int status { get; set; }
+    }
+}
